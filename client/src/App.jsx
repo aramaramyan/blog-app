4675ce6dfc7e0cf7bf025a01a24f88f5e21da940
aramaramyan <aramaramyan1997@mail.ru>
2022-10-01 14:03:21 +0400

    initialiaze Project

---
+++ client/src/App.jsx
@@ -0,0 +1,57 @@
+import {createBrowserRouter, Outlet, RouterProvider} from "react-router-dom";
+import Home from "./pages/Home";
+import Write from "./pages/Write";
+import Login from "./pages/Login";
+import Single from "./pages/Single";
+import Register from "./pages/Register";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
+const Layout = () => {
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </>
+  );
+};
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />
+      },
+      {
+        path: "/single",
+        element: <Single />
+      },
+      {
+        path: "/write",
+        element: <Write />
+      }
+    ]
+  },
+  {
+    path: "/register",
+    element: <Register />
+  },
+  {
+    path: "/login",
+    element: <Login />
+  }
+]);
+
+function App() {
+  return (
+    <div>
+      <RouterProvider router={router} />
+    </div>
+  );
+}
+
+export default App;
