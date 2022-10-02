f648bf40c1cca8d8c97825f974de249ab209fc35
aramaramyan <aramaramyan1997@mail.ru>
2022-10-02 14:06:55 +0400

    fix: some issue

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ app.use(express.json());
 app.listen(8800, () => {
   console.log(`
     🚀  Server is running!
-    🔉  Listening on port 4000
+    🔉  Listening on port 8800
     📭  Query at http://localhost:8800
 `);
 });
