b97d0c8fe7d4264c485627ba0e68ba256451655f
aramaramyan <aramaramyan1997@mail.ru>
2022-10-03 22:53:29 +0400

    feat: add login controller

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,13 +1,15 @@
 import bycript from "bcryptjs";
+import jwt from "jsonwebtoken";
 import { db } from "./../db.js";
 
 export const register = (req, res) => {
   // CHECK EXISTING USER
-  const query = "SELECT * FROM users WHERE email = ? OR username = ?";
+  const query = "SELECT * FROM users WHERE email = ?";
 
-  db.query(query, [req.body.email, req.body.username], (err, data) => {
+  db.query(query, [req.body.email], (err, data) => {
     if (err) return res.json(err);
-    if (data.length) return res.status(409).json("User already exists");
+    if (data.length)
+      return res.status(409).json("User with this email already exists");
 
     // Hash the password and create a user
     const salt = bycript.genSaltSync(10);
@@ -24,6 +26,32 @@ export const register = (req, res) => {
   });
 };
 
-export const login = (req, res) => {};
+export const login = (req, res) => {
+  // CHECK USER EMAIL
+
+  const query = "SELECT * FROM users WHERE email = ?";
+
+  db.query(query, [req.body.email], (err, data) => {
+    if (err) return res.json(err);
+    if (data.length === 0) return res.status(404).json("email");
+
+    // CHECK USER PASSWORD
+
+    const isPasswordCorrect = bycript.compareSync(
+      req.body.password,
+      data[0].password
+    );
+
+    if (!isPasswordCorrect) return res.status(400).json("password");
+
+    const token = jwt.sign({ id: data[0].id }, "jwtkey");
+    const { password, ...restData } = data;
+
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(200)
+      .json(restData);
+  });
+};
 
 export const logout = (req, res) => {};
