6b1ba2cd0940bc4234ac054117d858f42c994303
aramaramyan <aramaramyan1997@mail.ru>
2022-10-09 16:27:41 +0400

    feat: add useNavigate hook in Navbar component

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React, { useContext, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import { AuthContext } from "../context/authContext";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
@@ -13,7 +14,6 @@ import Button from "@mui/material/Button";
 import Tooltip from "@mui/material/Tooltip";
 import MenuItem from "@mui/material/MenuItem";
 import AdbIcon from "@mui/icons-material/Adb";
-import { Link } from "react-router-dom";
 
 const pages = ["ART", "SCIENCE", "TECHNOLOGY", "CINEMA", "DESIGN", "FOOD"];
 
@@ -21,6 +21,7 @@ const Navbar = () => {
   const { currentUser, logout } = useContext(AuthContext);
   const [anchorElNav, setAnchorElNav] = useState(null);
   const [anchorElUser, setAnchorElUser] = useState(null);
+  const navigate = useNavigate();
 
   const handleOpenNavMenu = (event) => {
     setAnchorElNav(event.currentTarget);
@@ -160,6 +161,7 @@ const Navbar = () => {
                   onClick={() => {
                     handleCloseUserMenu();
                     logout();
+                    navigate("/login");
                   }}
                 >
                   <Typography textAlign="center">Logout</Typography>
