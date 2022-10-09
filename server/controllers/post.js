dbf056664bc8170ac6ddc335c75cd58e169b9299
aramaramyan <aramaramyan1997@mail.ru>
2022-10-09 16:50:22 +0400

    feat: add getPosts controller

--- server/controllers/post.js
+++ server/controllers/post.js
@@ -1,5 +1,15 @@
+import { db } from "../db.js";
+
 export const getPosts = (req, res) => {
-  res.json("from controller");
+  const query = req.query.cat
+    ? "SELECT * FROM posts WHERE category=?"
+    : "SELECT * FROM posts";
+
+  db.query(query, [req.query.cat], (err, data) => {
+    if (err) return res.send(err);
+
+    return res.status(200).json(data);
+  });
 };
 
 export const getPost = (req, res) => {
