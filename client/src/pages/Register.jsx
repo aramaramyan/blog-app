54de625d5ed10e29029b5fecfc19ade969825949
aramaramyan <aramaramyan1997@mail.ru>
2022-10-29 17:04:24 +0400

    feat: add basic styles to Registration page

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -5,12 +5,29 @@ import { makeStyles } from "@mui/styles";
 import { AuthContext } from "../context/authContext";
 
 const useStyles = makeStyles({
+  registerPageWrapper: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    width: "900px",
+    height: "100vh",
+  },
+  registerWrapper: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "space-evenly",
+    alignItems: "center",
+    gap: 10,
+    width: "300px",
+    height: "400px",
+    borderRadius: 4,
+    border: "1px solid blue",
+  },
   formContainer: {
     display: "flex",
     flexDirection: "column",
     justifyContent: "center",
     alignItems: "center",
-    border: "1px solid black",
     height: "50%",
   },
 });
@@ -40,52 +57,54 @@ const Register = () => {
   };
 
   return (
-    <Stack direction="column" spacing={2}>
-      <h1>Login</h1>
-      <form className={classes.formContainer}>
-        <TextField
-          required
-          id="standard-name-input"
-          label="Full Name"
-          type="text"
-          name="userName"
-          value={inputs.userName}
-          autoComplete="current-name"
-          variant="standard"
-          onChange={handleChange}
-        />
-        <TextField
-          required
-          id="standard-email-input"
-          label="Email"
-          type="text"
-          name="email"
-          value={inputs.email}
-          autoComplete="current-email"
-          variant="standard"
-          error={!!error}
-          helperText={error}
-          onChange={handleChange}
-        />
-        <TextField
-          required
-          id="standard-password-input"
-          label="Password"
-          type="password"
-          name="password"
-          value={inputs.password}
-          autoComplete="current-password"
-          variant="standard"
-          onChange={handleChange}
-        />
-        <Button variant="outlined" onClick={handleSubmit}>
-          register
-        </Button>
-        <span>
-          Do you have an account? <Link to="/login">Login</Link>
-        </span>
-      </form>
-    </Stack>
+    <div className={classes.registerPageWrapper}>
+      <div className={classes.registerWrapper}>
+        <h1>Login</h1>
+        <form className={classes.formContainer}>
+          <TextField
+            required
+            id="standard-name-input"
+            label="Full Name"
+            type="text"
+            name="userName"
+            value={inputs.userName}
+            autoComplete="current-name"
+            variant="standard"
+            onChange={handleChange}
+          />
+          <TextField
+            required
+            id="standard-email-input"
+            label="Email"
+            type="text"
+            name="email"
+            value={inputs.email}
+            autoComplete="current-email"
+            variant="standard"
+            error={!!error}
+            helperText={error}
+            onChange={handleChange}
+          />
+          <TextField
+            required
+            id="standard-password-input"
+            label="Password"
+            type="password"
+            name="password"
+            value={inputs.password}
+            autoComplete="current-password"
+            variant="standard"
+            onChange={handleChange}
+          />
+          <Button variant="outlined" onClick={handleSubmit}>
+            register
+          </Button>
+          <span>
+            Do you have an account? <Link to="/login">Login</Link>
+          </span>
+        </form>
+      </div>
+    </div>
   );
 };
 
